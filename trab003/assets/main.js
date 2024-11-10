cb95f76521801117a1f7435b9970024406fd2df6
Gustavo Jorge <gustavojorgehz@gmail.com>
2024-11-10 17:34:33 -0300

    Add modal

--- trab003/assets/main.js
+++ trab003/assets/main.js
@@ -1,4 +1,35 @@
-const bar = document.querySelector("aside")
-bar.addEventListener("click" , () => {
-    bar.classList.toggle("openbar")
+const galleryImageModal = document.querySelector('.gallery-image-modal')
+const galleryBtClose = galleryImageModal.querySelector('.bt-close')
+const galleryContent = galleryImageModal.querySelector('.content')
+const imgs = document.querySelectorAll('.gallery img')
+
+galleryBtClose.addEventListener('click', () => {
+  galleryImageModal.close()
+})
+
+imgs.forEach(img => {
+  img.addEventListener('click', () => {
+    galleryContent.innerHTML = `<img src="${img.src}">`
+    galleryImageModal.showModal()
+  })
+})
+
+const galleryBtNext = galleryImageModal.querySelector('.bt-next')
+let nextImg = null
+
+galleryBtNext.addEventListener('click', () => {
+  galleryImageModal.close()
+  nextImg.click()
+})
+
+galleryBtClose.addEventListener('click', () => {
+  galleryImageModal.close()
+})
+
+imgs.forEach(img => {
+  img.addEventListener('click', () => {
+    nextImg = img.parentNode.nextElementSibling.querySelector('img')
+    galleryContent.innerHTML = `<img src="${img.src}">`
+    galleryImageModal.showModal()
+  })
 })
